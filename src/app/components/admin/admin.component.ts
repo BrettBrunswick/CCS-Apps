8257bd97112fb2f2c05ed45514a2df7cf130b640
unknown <brettbrun@gmail.com>
2018-11-06 23:29:20 -0500

    added datatables

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,21 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { User } from '../../models/User';
 import { DataService } from 'src/app/services/data.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnInit, OnDestroy {
 
   public allUsers: User[];
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<User[]> = new Subject();
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
+
+    this.dtOptions = {
+      columnDefs: [{
+        targets:[2],
+        orderable: false
+      }]
+    };
+
     this.dataService.getAllUsers()
-        .subscribe(data => this.allUsers = data);
+        .subscribe(data => {
+          this.allUsers = data
+          this.dtTrigger.next()
+        });
+
+  }
+
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
   }
 
 }

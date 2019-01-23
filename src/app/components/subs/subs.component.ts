78b3f8912ae24ffd025b711763ba5134bed6fdad
Brett Brunswick <BrettBrunswick@Bretts-MBP-2.attlocal.net>
2019-01-22 20:53:57 -0500

    progress on subs component

--- src/app/components/subs/subs.component.ts
+++ src/app/components/subs/subs.component.ts
@@ -16,6 +16,8 @@ export class SubsComponent implements OnInit {
   sub: SubContractor = new SubContractor();
   location: Location = new Location();
 
+  showSpinner = true;
+
   constructor(private dataService: DataService, private route: ActivatedRoute) { }
 
   ngOnInit() 
@@ -29,18 +31,20 @@ export class SubsComponent implements OnInit {
     const subId = +this.route.snapshot.paramMap.get('id');
     this.dataService.getSubById(subId)
       .subscribe(data => {
-        this.sub.Name = data['name'],
-        this.sub.AddressLine1 = data['addressLine1'],
-        this.sub.AddressLine2 = data['addressLine2'],
-        this.sub.State = data['state'],
-        this.sub.City = data['city'],
-        this.sub.ZipCode = data['zipCode'],
-        this.sub.WebsiteURL = data['websiteURL'],
-        this.sub.OfficePhone = data['officePhone'],
-        this.sub.OfficeEmail = data['officeEmail'],
-        this.sub.OfficeFax = data['officeFax'],
-        this.sub.Source = data['source'],
-        this.sub.TradeName = data['trade.name']
+        this.sub.Name = data['name'];
+        this.sub.AddressLine1 = data['addressLine1'];
+        this.sub.AddressLine2 = data['addressLine2'];
+        this.sub.State = data['state'];
+        this.sub.City = data['city'];
+        this.sub.ZipCode = data['zipCode'];
+        this.sub.WebsiteURL = data['websiteURL'];
+        this.sub.OfficePhone = data['officePhone'];
+        this.sub.OfficeEmail = data['officeEmail'];
+        this.sub.OfficeFax = data['officeFax'];
+        this.sub.Source = data['source'];
+        this.sub.TradeName = data['trade']['name'];
+        this.showSpinner = false;
+        console.log(this.sub)
         this.getLocation();
       });
   }
@@ -49,8 +53,8 @@ export class SubsComponent implements OnInit {
   {
     this.dataService.getLocationByZip(this.sub.ZipCode)
       .subscribe(data => {
-        this.location.Latitude = data[0]['latitude'],
-        this.location.Longitute = data[0]['longitude'],
+        this.location.Latitude = data[0]['latitude'];
+        this.location.Longitute = data[0]['longitude'];
         console.log(this.location);
     });
   }
@@ -58,6 +62,31 @@ export class SubsComponent implements OnInit {
   isLocationDefined(): boolean
   {
     return (this.location.Longitute != undefined && this.location.Latitude != undefined);
+  }
+
+  hasAddressLine1()
+  {
+    return !this.dataService.isBlankOrNull(this.sub.AddressLine1);
+  }
+
+  hasAddressLine2()
+  {
+    return !this.dataService.isBlankOrNull(this.sub.AddressLine2);
+  }
+
+  hasCity()
+  {
+    return !this.dataService.isBlankOrNull(this.sub.City);
+  }
+
+  hasState()
+  {
+    return !this.dataService.isBlankOrNull(this.sub.State);
+  }
+
+  hasZipCode()
+  {
+    return !this.dataService.isBlankOrNull(this.sub.ZipCode);
   }
 
 }

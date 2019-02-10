da42acb2f74f116b31914383813d361f9d4533b8
Brett Brunswick <BrettBrunswick@Bretts-MBP-2.attlocal.net>
2019-02-10 17:11:21 -0500

    only show footer if logged in

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  isUserLoggedIn(): boolean
+  {
+    return this.userService.isUserLoggedIn();
   }
 
 }
